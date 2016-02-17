15ce4962609c419b4c551163aa94345792e7e284
LKM <kwan@k2mediawebdesign.com>
2016-02-17 11:47:53 -0500

    more placeholders for tool ideas

---
+++ src/WebDevTools/Controllers/ToolsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebDevTools.Controllers
+{
+    public class ToolsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ServerSniffer()
+        {
+            return View();
+        }
+    }
+}
