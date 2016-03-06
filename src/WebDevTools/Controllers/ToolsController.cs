5b5ae532a94670967339725e30311328c2466759
LKM <kwan@k2mediawebdesign.com>
2016-03-06 10:49:47 -0500

    Add HtmlAgilityPack.NetCore to parse the html and figure out what libraries are being used

--- src/WebDevTools/Controllers/ToolsController.cs
+++ src/WebDevTools/Controllers/ToolsController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-
+using HtmlAgilityPack;
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebDevTools.Controllers
@@ -17,15 +17,9 @@ namespace WebDevTools.Controllers
             return View();
         }
 
-        public IActionResult ServerSniffer()
-        {
-            return View();
-        }
-
-        public IActionResult BuiltWith()
-        {
-            return View();
-        }
+        // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        // -- Http server sniffer
+        // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
         class HttpHeaders
         {
@@ -33,10 +27,16 @@ namespace WebDevTools.Controllers
             public Dictionary<string, string> ResponseHeaders { get; set; }
         }
 
+        public IActionResult ServerSniffer()
+        {
+            return View();
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> ServerSniffer(string uri)
         {
-            
+
             var request_headers = new Dictionary<string, string>();
             var response_headers = new Dictionary<string, string>();
 
@@ -48,7 +48,7 @@ namespace WebDevTools.Controllers
                 var request = new HttpRequestMessage(HttpMethod.Head, uri);
                 using (var response = await http_client.SendAsync(request))
                 {
-                    foreach(var header in response.Headers)
+                    foreach (var header in response.Headers)
                     {
                         response_headers.Add(header.Key, string.Join(",", header.Value));
                     }
@@ -61,5 +61,40 @@ namespace WebDevTools.Controllers
             var json_result = new JsonResult(http_headers);
             return json_result;
         }
+
+        // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        // -- Page Built With
+        // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        class BuiltWithInfo
+        {
+            public List<string> Scripts = new List<string>();
+            public List<string> ThirdPartyLibraries = new List<string>();
+            public List<string> Stylesheets = new List<string>();
+        }
+
+        public IActionResult BuiltWith()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BuiltWith(string uri)
+        {
+
+            var info = new BuiltWithInfo();
+            using (var http_client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, uri);
+                using (var response = await http_client.SendAsync(request))
+                {
+                    HtmlDocument doc = new HtmlDocument();
+               
+                }
+            }
+            var json_result = new JsonResult(info);
+            return json_result;
+        }
+
     }
 }
